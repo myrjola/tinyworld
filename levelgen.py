eb9d9ea253ca1fd9ef8f5b04f41128fa12372f55
Martin Yrjölä <martin.yrjola@gmail.com>
2008-05-11 12:38:10 +0000

    functionalized the levelmaker

--- levelgen.py
+++ levelgen.py
@@ -14,18 +14,19 @@ charlist = []
 badguylist = []
 platflist = []
 wallist = []
-level = str(raw_input('which level name? --> '))
-
 
 pygame.init()
 screen = pygame.display.set_mode([1024,768])
 background = pygame.Surface([1024,768])
 background.fill([255,255,255])
-screen.blit(background,[0,0])
+levelmapimg = load_png('levelmap.png')[0]
+screen.blit(levelmapimg,[0,0])
 pygame.display.flip()
 running = 1
 objsprites = pygame.sprite.Group()
 mousesprite = pygame.sprite.RenderUpdates()
+mousepos = (0,0)
+
 
 class mouseSprite(pygame.sprite.Sprite):
     def __init__(self, (image,rect)):
@@ -48,58 +49,77 @@ def alignToGrid((x,y)):
     xout = x/32*32
     yout = y/32*32
     return (xout,yout)
-imgrect = load_png(spritelist[listorderindex])
-mousesprite.add(mouseSprite(imgrect))
 
-while running == 1:
-    if pygame.mouse.get_focused:
-        mousepos = alignToGrid(pygame.mouse.get_pos())
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = 0
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    level = str(pygame.mouse.get_pos()[0]/64) + \
-                            str(pygame.mouse.get_pos()[1]/64) 
-                    print "You chose: ", level
-                    running = 0
- 
-running = 1
+def chooseLevel():
+    #Choose the level from the grid
+    while 1:
+        if pygame.mouse.get_focused:
 
-while running == 1:
-    if pygame.mouse.get_focused:
-        mousepos = alignToGrid(pygame.mouse.get_pos())
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = 0
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    mousepos = alignToGrid(pygame.mouse.get_pos())
-                    leveldatadict[listorder[listorderindex]].append(mousepos)
-                    background.blit(imgOfObject(imgrect,mousepos).image, mousepos)
-#                    objsprites.add(imgOfObject(imgrect,mousepos))
-                if event.button == 3:
-                    listorderindex += 1
-                    try: 
-                        print listorder[listorderindex]
-                        imgrect = load_png(spritelist[listorderindex])
-                        mousesprite.update(imgrect[0])
-                    except IndexError: 
-                        listorderindex = 1
-                        imgrect = load_png(spritelist[listorderindex])
-                        mousesprite.update(imgrect[0])
+            mousepos = alignToGrid(pygame.mouse.get_pos())
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    raise SystemExitStatus('message')
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        global level
+                        level = str(pygame.mouse.get_pos()[0]/64) + \
+                                str(pygame.mouse.get_pos()[1]/64) 
+                        print "You chose: ", level
+                        return
+    
+# Make the screen ready for mapmaking
+def makeLevel(): 
+    running = 1
+    screen.blit(background,[0,0])
+    pygame.display.flip()
+    listorder = ["mainchar","badguys","balls","platforms","walls"]
+    spritelist = ["char2.png","badguy1.png","ball1.png","solid.png","wall.png"]
+    leveldatadict = {"mainchar":[],"badguys":[],"balls":[],"platforms":[],"walls":[]}
+    listorderindex = 0
+    charlist = []
+    badguylist = []
+    platflist = []
+    wallist = []
+    imgrect = load_png(spritelist[listorderindex])
+    mousesprite.add(mouseSprite(imgrect))
+    global mousepos
+    global leveldatadict
+
+    # The levelmaking interface
+    while 1:
+        if pygame.mouse.get_focused:
+            mousepos = alignToGrid(pygame.mouse.get_pos())
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        mousepos = alignToGrid(pygame.mouse.get_pos())
+                        leveldatadict[listorder[listorderindex]].append(mousepos)
+                        background.blit(imgOfObject(imgrect,mousepos).image, mousepos)
+    #                    objsprites.add(imgOfObject(imgrect,mousepos))
+                    if event.button == 3:
+                        listorderindex += 1
+                        try: 
+                            print listorder[listorderindex]
+                            imgrect = load_png(spritelist[listorderindex])
+                            mousesprite.update(imgrect[0])
+                        except IndexError: 
+                            listorderindex = 1
+                            imgrect = load_png(spritelist[listorderindex])
+                            mousesprite.update(imgrect[0])
 
 
-                print event.button
-                print pygame.mouse.get_pos()
-    mousesprite.update()
-    objsprites.draw(background)
-    screen.blit(background,(0,0))
-    pygame.display.update(mousesprite.draw(screen))
-print leveldatadict
+                    print event.button
+                    print pygame.mouse.get_pos()
+        
+        mousesprite.update()
+        objsprites.draw(background)
+        screen.blit(background,(0,0))
+        pygame.display.update(mousesprite.draw(screen))
+    print leveldatadict
 
 
-#TODO make mouse driven level editor
 
 
 '''
@@ -108,10 +128,24 @@ badguylist.append([400,200])
 platformlist = [[390,600],[390,300],[390,390]]
 wallist = [[390,300],[700,300]]
 '''
+#Start levelmaking
+chooseLevel()
+makeLevel()
 
 
-fullname = os.path.join('levels',level)
-levelfile = open(fullname, 'w')
+#Save image of level
+imagepath = os.path.join('images','levelmap.png')
+minimg = pygame.transform.scale(background,(64,64))
+levelmapimg.blit(minimg,[int(level[0])*64,int(level[1])*64])
+pygame.image.save(levelmapimg,imagepath)
+
+
+
+
+#Save level
+print leveldatadict
+fullpath = os.path.join('levels',level)
+levelfile = open(fullpath, 'w')
 pickle.dump(leveldatadict, levelfile)
 print "level generated"
 
